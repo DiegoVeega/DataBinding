5ff2d7bb7afca911dddf3efcb9e4374f7a17f0a6
DiegoVeega <00148816@uca.edu.sv>
2019-05-08 00:44:47 -0600

    intento aplicacion dataBinding

--- app/src/main/java/com/genesis/databindingteams/MainActivity.kt
+++ app/src/main/java/com/genesis/databindingteams/MainActivity.kt
@@ -2,12 +2,48 @@ package com.genesis.databindingteams
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //ActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+
+
+    }
+
+    fun addOneA(){
+
+    }
+
+    fun addOneB(){
+
+    }
+
+    fun addTwoA(){
+
+    }
+
+    fun addTwoB(){
+
+    }
+
+    fun addThreeA(){
+
+    }
+
+    fun addThreeB(){
+
+    }
+
+    fun reset(){
+
     }
 
 }
