1c6f160a1cd33f66d194b350f72177426e96c902
DiegoVeega <00148816@uca.edu.sv>
2019-05-07 23:39:15 -0600

    Initial commit - XML principal de la aplicacion, modelo creado.

---
+++ app/src/main/java/com/genesis/databindingteams/PuntosEquipo.kt
@@ -0,0 +1,12 @@
+package com.genesis.databindingteams
+
+class PuntosEquipo {
+
+    var puntosEquipoA:Int? = 0
+    var puntosEquipoB:Int? = 0
+
+    constructor(puntosEquipoA: Int?, puntosEquipoB: Int?) {
+        this.puntosEquipoA = puntosEquipoA
+        this.puntosEquipoB = puntosEquipoB
+    }
+}
